cbb8870f5e1078fdf8f7a1d545d15f2647a260c7
berenisser <berenisser@gmail.com>
2017-07-23 20:57:55 -0400

    todos los cambios listos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+   	var nav = $('.navi').offset().top;
+   	$(document).scroll(function() { 
+      if($(this).scrollTop() > nav) {
+          $('.navi').css({'background-color': '#fff'});
+          $('.navi').css({'box-shadow': '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'});
+          $('.gris').css({'color': '#555555'});
+       } else {
+          $('.navi').css({'background-color': 'transparent'});
+          $('.gris').css({'color': '#fff'});
+          $('.navi').css({'box-shadow': 'none'});
+       }
+   	});
+
 	$(".dropdown-button").dropdown(
 		{hover: true}
 	);
